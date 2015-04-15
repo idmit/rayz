ebee29c65ea70df60293ef38f2cfa9070b3732e9
Ivan Dmitrievsky <ivan.dmitrievsky@gmail.com>
2015-04-15 15:21:46 +0300

    Implement triangle intersection

--- src/triangle.cpp
+++ src/triangle.cpp
@@ -11,6 +11,27 @@
 triangle::triangle(std::array<dvec3, 3> vertices) : _vertices(vertices){};
 
 bool triangle::intersect(dmat4 lcs, ray ray, dvec3 *intersection_point) const {
+  dvec3 n =
+      glm::cross((_vertices[1] - _vertices[0]), _vertices[2] - _vertices[0]);
+  double t = -glm::dot((ray.origin - _vertices[0]), n) / glm::dot(ray.dir, n);
+
+  *intersection_point = ray.origin + ray.dir * t;
+
+  if (glm::dot(glm::cross(_vertices[1] - _vertices[0],
+                          *intersection_point - _vertices[0]),
+               n) < 0) {
+    return false;
+  }
+  if (glm::dot(glm::cross(_vertices[2] - _vertices[1],
+                          *intersection_point - _vertices[1]),
+               n) < 0) {
+    return false;
+  }
+  if (glm::dot(glm::cross(_vertices[0] - _vertices[2],
+                          *intersection_point - _vertices[2]),
+               n) < 0) {
+    return false;
+  }
   return true;
 }
 
